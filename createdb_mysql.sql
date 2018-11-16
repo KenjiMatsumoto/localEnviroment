aae4a79ad9fb0d23e74fb2e64f1c433593669e1a
Kenji Matsumoto <kenjimatsumoto1983@gmail.com>
2018-11-16 10:44:50 +0900

    Update createdb_mysql.sql

--- createdb_mysql.sql
+++ createdb_mysql.sql
@@ -1,3 +1,10 @@
 CREATE USER mydb_ms IDENTIFIED BY [PASSWORD] 'mydb_ms';
-CREATE DATABASE mydb_ms;
-GRNANT ALL ON db_name.* TO mydb_ms;
+--初期設定（ユーザー作成とDBの作成、権限設定）
+CREATE ROLE mydb_ps@localhost;
+SET PASSWORD FOR mydb_ms@localhost=PASSWORD('mydb_ms');
+ALTER ROLE mydb_ps WITH LOGIN;
+--ロールをユーザーに割り当て
+GRANT mydb_ms@localhost TO mydb_ms@localhost;
+CREATE TABLESPACE mydb_ms location '/var/lib/mysql/mydb_ms';
+GRANT ALL ON TABLESPACE mydb_ms to mydb_ms;
+GRANT ALL PRIVILEGES ON *.* TO mydb_ms IDENTIFIED BY 'mydb_ms' WITH GRANT OPTION;
