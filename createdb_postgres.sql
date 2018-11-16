ef4dfb3628e64b78573aaf7425400cac82d9fce0
Kenji Matsumoto <kenjimatsumoto1983@gmail.com>
2018-11-16 10:31:17 +0900

    postgresの諸々の設定
    追記

--- createdb_postgres.sql
+++ createdb_postgres.sql
@@ -1,4 +1,8 @@
 --初期設定（ユーザー作成とDBの作成、権限設定）
-CREATE USER mydb_ps;
-CREATE DATABASE mydb_ps;
+CREATE ROLE mydb_ps;
+ALTER ROLE mydb_ps WITH PASSWORD 'mydb_ps';
+ALTER ROLE mydb_ps WITH LOGIN;
+CREATE TABLESPACE mydb_ps location '/var/lib/postgresql/mydb_ps';
+GRANT ALL ON TABLESPACE mydb_ps to mydb_ps;
+ALTER USER mydb_ps WITH SUPERUSER;
 GRANT ALL PRIVILEGES ON DATABASE mydb_ps TO mydb_ps;
