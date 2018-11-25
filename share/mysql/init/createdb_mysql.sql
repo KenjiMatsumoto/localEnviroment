0efde695db1336be88fc461875689b7a7ff78575
Kenji Matsumoto <kenjimatsumoto1983@gmail.com>
2018-11-26 08:59:32 +0900

    Update createdb_mysql.sql
    Grant文修正

--- share/mysql/init/createdb_mysql.sql
+++ share/mysql/init/createdb_mysql.sql
@@ -1,4 +1,4 @@
 CREATE DATABASE IF NOT EXISTS mydb_ms;
 CREATE USER 'mydb_ms'@'localhost' identified by 'mydb_ms';
-GRANT ALL PRIVILEGES ON *.* TO 'mydb_ms'@'localhost';
+GRANT ALL PRIVILEGES ON *.* TO 'mydb_ms'@'localhost' identified by 'mydb_ms' with grant option;
 FLUSH PRIVILEGES;
